122d0168af50fcbdcf087db78a301ea5817cbaff
Sergey <osipovsergei97@gmail.com>
2018-10-29 23:31:27 +0300

    Add jquery.js

---
+++ dist/js/jquery.js
@@ -0,0 +1,29 @@
+$( document ).ready( function() {
+    var $quickContactInfo = $('.b-quick-contact-info');
+    var $infoTip          = $('.b-information-tip');
+    var $toggleButton     = $('.b-toggle-button');
+    var $menuWrap         = $('.b-menu');
+    var url               = document.location.href;
+
+
+    $quickContactInfo.on( 'click', function() {
+        'use strict';
+
+        $infoTip.toggleClass('b-information-tip-open');
+    } );
+
+    $.each( $('li a'), function() {
+        'use strict';
+
+        if ( this.href == url ) {
+            $( this ).addClass('activeLink');
+        }
+    } );
+
+    $toggleButton.on( 'click', function() {
+        'use strict';
+
+        $( this ).toggleClass('b-button-open');
+        $menuWrap.toggleClass('b-menu-open');
+    } );
+} );
