9207250db91d611fe5643dbaa7f280cc2bc62eaf
Sergey <osipovsergei97@gmail.com>
2018-11-24 16:03:14 +0300

    Update jquery.js

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -3,7 +3,42 @@ $( document ).ready( function() {
     var $infoTip          = $('.b-information-tip');
     var $toggleButton     = $('.b-toggle-button');
     var $menuWrap         = $('.b-menu');
+    var currIndex;
     var url               = document.location.href;
+
+    $quickContactInfo.on( 'click', function() {
+        'use strict';
+
+        $infoTip.toggleClass('b-information-tip-open');
+    } );
+
+    $.each( $('li a'), function() {
+        'use strict';
+
+        if ( this.href == url ) {
+            $( this ).addClass('activeLink');
+        }
+    } );
+
+    $toggleButton.on( 'click', function() {
+        'use strict';
+
+        $( this ).toggleClass('b-button-open');
+        $menuWrap.toggleClass('b-menu-open');
+    } );
+
+    // Slider for work.html and blog-post.html
+
+    $('.b-post__main-image__img:first').fadeIn();
+    $('.b-rectangle').click( function() {
+        $('.b-rectangle').removeClass('b-selected');
+        $( this ).addClass('b-selected');
+        currIndex = $( this ).index() + 1;
+        $('.b-post__main-image__img').fadeOut( 0 );
+        $( '.b-post__main-image__img:nth-child(' + currIndex + ')' ).fadeIn();
+    } );
+
+    // Slider for blog.html
 
     $('a.b-preview-slide').click( function() {
         previewSlide( $( this ).parents('.b-slider').find('.b-slides') );
@@ -48,25 +83,4 @@ $( document ).ready( function() {
 
         $slides.find('img:first').fadeIn( 1000 );
     }
-
-    $quickContactInfo.on( 'click', function() {
-        'use strict';
-
-        $infoTip.toggleClass('b-information-tip-open');
-    } );
-
-    $.each( $('li a'), function() {
-        'use strict';
-
-        if ( this.href == url ) {
-            $( this ).addClass('activeLink');
-        }
-    } );
-
-    $toggleButton.on( 'click', function() {
-        'use strict';
-
-        $( this ).toggleClass('b-button-open');
-        $menuWrap.toggleClass('b-menu-open');
-    } );
 } );
