66acecd945751d4fe9b7ad326b092576e7b14bf0
Sergey <osipovsergei97@gmail.com>
2018-11-24 04:08:17 +0300

    Update jquery.js

--- dist/js/jquery.js
+++ dist/js/jquery.js
@@ -5,6 +5,49 @@ $( document ).ready( function() {
     var $menuWrap         = $('.b-menu');
     var url               = document.location.href;
 
+    $('a.b-preview-slide').click( function() {
+        previewSlide( $( this ).parents('.b-slider').find('.b-slides') );
+    } );
+
+    // clicking image goes to next slide
+
+    $('a.b-next-slide, .b-slider img').click( function() {
+        nextSlide( $( this ).parents('.b-slider').find('.b-slides') );
+    } );
+
+    // initialize show
+
+    iniShow();
+
+    function iniShow() {
+
+    // show first image
+
+        $('.b-slider').each( function() {
+            $( this ).find('img:first').fadeIn( 1000 );
+        } );
+    }
+
+    function previewSlide( $slides ) {
+        $slides.find('img:last').prependTo( $slides );
+        showSlide( $slides );
+    }
+
+    function nextSlide( $slides ) {
+        $slides.find('img:first').appendTo( $slides );
+        showSlide( $slides );
+    }
+
+    function showSlide( $slides ) {
+
+    // hide (reset) all slides
+
+        $slides.find('img').hide();
+
+        // fade in next slide
+
+        $slides.find('img:first').fadeIn( 1000 );
+    }
 
     $quickContactInfo.on( 'click', function() {
         'use strict';
