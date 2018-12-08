68030d2dc8d45a0b2d0ae969c143a99bf09eada4
Sergey <osipovsergei97@gmail.com>
2018-12-08 23:04:22 +0300

    Update jquery.js

--- dist/js/jquery.js
+++ dist/js/jquery.js
@@ -3,8 +3,8 @@ $( document ).ready( function() {
     var $infoTip          = $('.b-information-tip');
     var $toggleButton     = $('.b-toggle-button');
     var $menuWrap         = $('.b-menu');
+    var currIndex;
     var url               = document.location.href;
-
 
     $quickContactInfo.on( 'click', function() {
         'use strict';
@@ -26,5 +26,61 @@ $( document ).ready( function() {
         $( this ).toggleClass('b-button-open');
         $menuWrap.toggleClass('b-menu-open');
     } );
-} );
 
+    // Slider for work.html and blog-post.html
+
+    $('.b-post__slide:first').fadeIn();
+    $('.b-rectangle').click( function() {
+        $('.b-rectangle').removeClass('b-selected');
+        $( this ).addClass('b-selected');
+        currIndex = $( this ).index() + 1;
+        $('.b-post__slide').fadeOut( 0 );
+        $( '.b-post__slide:nth-child(' + currIndex + ')' ).fadeIn();
+    } );
+
+    // Slider for blog.html
+
+    $('a.b-preview-slide').click( function() {
+        previewSlide( $( this ).parents('.b-gallery-post__slider-wrap').find('.b-slides') );
+    } );
+
+    // clicking image goes to next slide
+
+    $('a.b-next-slide, .b-gallery-post__slider-wrap img').click( function() {
+        nextSlide( $( this ).parents('.b-gallery-post__slider-wrap').find('.b-slides') );
+    } );
+
+    // initialize show
+
+    iniShow();
+
+    function iniShow() {
+
+    // show first image
+
+        $('.b-gallery-post__slider-wrap').each( function() {
+            $( this ).find('img:first').fadeIn( 1000 );
+        } );
+    }
+
+    function previewSlide( $slides ) {
+        $slides.find('img:last').prependTo( $slides );
+        showSlide( $slides );
+    }
+
+    function nextSlide( $slides ) {
+        $slides.find('img:first').appendTo( $slides );
+        showSlide( $slides );
+    }
+
+    function showSlide( $slides ) {
+
+    // hide (reset) all slides
+
+        $slides.find('img').hide();
+
+        // fade in next slide
+
+        $slides.find('img:first').fadeIn( 1000 );
+    }
+} );
